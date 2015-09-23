7b9a08ca7261a8ad06eab6b2cf618415ff548fc8
Shengying Pan <i@thepans.me>
2015-09-23 11:57:52 -0400

    added a new project

---
+++ AppStudent/app/src/main/java/com/qa/appstudent/views/SoundView.java
@@ -0,0 +1,14 @@
+package com.qa.appstudent.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class SoundView extends View {
+
+}
