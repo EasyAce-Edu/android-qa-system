d8278c58ace087f8fae97a1849add54b2d2979fd
Shengying Pan <i@thepans.me>
2015-09-23 20:48:34 -0400

    fuck myself

--- AppStudent/app/src/main/java/com/qa/appstudent/SoundView.java
+++ AppStudent/app/src/main/java/com/qa/appstudent/SoundView.java
@@ -11,6 +11,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -104,7 +105,7 @@ public class SoundView extends LinearLayout {
 					isRecording = false;
 					isDoneRecording = false;
 					buttonVoice.setText("RECORD");
-					buttonDelete.setVisibility(INVISIBLE);
+					buttonDelete.setVisibility(GONE);
 				}
 			}
 		});
